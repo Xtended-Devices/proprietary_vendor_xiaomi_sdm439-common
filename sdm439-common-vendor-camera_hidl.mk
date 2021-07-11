ef67b76dbd081956c6b952039e3e2f5237580431
zqm64 <zqm64@outlook.com>
2021-07-11 12:36:47 +0800

    sdm439-common: Drop stock camera HIDL blobs

--- sdm439-common-vendor-camera_hidl.mk
+++
@@ -1,5 +0,0 @@
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/sdm439-common/camera_hidl/vendor/lib64/android.hardware.camera.provider@2.4-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.camera.provider@2.4-legacy.so \
-    vendor/xiaomi/sdm439-common/camera_hidl/vendor/lib64/camera.device@3.2-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/camera.device@3.2-impl.so \
-    vendor/xiaomi/sdm439-common/camera_hidl/vendor/lib/android.hardware.camera.provider@2.4-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-legacy.so \
-    vendor/xiaomi/sdm439-common/camera_hidl/vendor/lib/camera.device@3.2-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera.device@3.2-impl.so
